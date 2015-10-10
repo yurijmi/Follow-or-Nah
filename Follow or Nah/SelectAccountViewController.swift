8d6db3ac1fd5316eb2c445f5eb4ff76450fcbfbc
Yuri Mikhaylov <me@yurijmi.ru>
2015-10-10 11:45:03 +0300

    Added Select Account View

---
+++ Follow or Nah/SelectAccountViewController.swift
@@ -0,0 +1,19 @@
+//
+//  SelectAccountViewController.swift
+//  Follow or Nah
+//
+//  Created by Юрий Михайлов on 10.10.15.
+//  Copyright © 2015 Юрий Михайлов. All rights reserved.
+//
+
+import UIKit
+
+class SelectAccountViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+
+}
