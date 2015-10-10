315f732f81e0e66bb87be7f20dc3065cb5d633e7
Yuri Mikhaylov <me@yurijmi.ru>
2015-10-10 09:39:24 +0300

    Initial Commit

---
+++ Follow or Nah/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Follow or Nah
+//
+//  Created by Юрий Михайлов on 10.10.15.
+//  Copyright © 2015 Юрий Михайлов. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
