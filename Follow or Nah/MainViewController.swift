130da4d2fba80f57678a99b843e542a404a666f5
Yuri Mikhaylov <me@yurijmi.ru>
2015-10-10 15:11:07 +0300

    Settings following count in Main View

--- Follow or Nah/MainViewController.swift
+++ Follow or Nah/MainViewController.swift
@@ -18,12 +18,34 @@ class MainViewController: UIViewController {
     @IBOutlet weak var followersLabel  : UILabel!
     @IBOutlet weak var followsYouLabel : UILabel!
     
-    var account : ACAccount?
+    var account    : ACAccount?
+    var twitterApi : TwitterApi?
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        self.twitterApi = TwitterApi(account: self.account!)
         
+        getFollowersCount()
+        getFriends()
+    }
+    
+    func getFollowersCount() {
+        self.twitterApi!.performQuery("https://api.twitter.com/1.1/account/verify_credentials.json", parameters: ["include_entities": "false", "skip_status": "true", "include_email": "false"],
+            handler: { (data :NSData!, response :NSHTTPURLResponse!, error :NSError!) -> Void in
+            if error == nil {
+                do {
+                    let response = try NSJSONSerialization.JSONObjectWithData(data, options: NSJSONReadingOptions.MutableLeaves) as! [String : AnyObject]
+                    
+                    let friendCount = response["friends_count"] as! Int
+                    
+                    dispatch_async(dispatch_get_main_queue()) {
+                        self.headingLabel!.text = "You follow \(friendCount) peeps and this dude"
+                    }
+                } catch {}
+            }
+        })
+    }
     }
     
     @IBAction func unfollowTapped(button: UIButton) {
