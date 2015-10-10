f0663680fd90e8463e4b4576f80cddaf37c44e24
Yuri Mikhaylov <me@yurijmi.ru>
2015-10-10 14:44:52 +0300

    Created outlets for Man View

--- Follow or Nah/MainViewController.swift
+++ Follow or Nah/MainViewController.swift
@@ -9,10 +9,24 @@
 import UIKit
 
 class MainViewController: UIViewController {
+    
+    @IBOutlet weak var headingLabel    : UILabel!
+    @IBOutlet weak var usernameLabel   : UILabel!
+    @IBOutlet weak var imageView       : UIImageView!
+    @IBOutlet weak var followersLabel  : UILabel!
+    @IBOutlet weak var followsYouLabel : UILabel!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        
+    }
+    
+    @IBAction func unfollowTapped(button: UIButton) {
+        
+    }
+    
+    @IBAction func followTapped(button: UIButton) {
         
     }
 
