2b6794f2c3ad5b844e168369b3c1968bd4bb03b2
Yuri Mikhaylov <me@yurijmi.ru>
2015-10-11 17:20:54 +0300

    Created End Credits View Controller with outlets and actions

---
+++ Follow or Nah/EndCreditsViewController.swift
@@ -0,0 +1,34 @@
+//
+//  EndCreditsViewController.swift
+//  Follow or Nah
+//
+//  Created by Юрий Михайлов on 11.10.15.
+//  Copyright © 2015 Юрий Михайлов. All rights reserved.
+//
+
+import UIKit
+
+class EndCreditsViewController: UIViewController {
+    
+    @IBOutlet weak var textView  : UITextView!
+    @IBOutlet weak var buyButton : UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+    }
+    
+    @IBAction func followTapped(button: UIButton) {
+        
+    }
+    
+    @IBAction func starTapped(button: UIButton) {
+        
+    }
+    
+    @IBAction func buyTapped(button: UIButton) {
+        
+    }
+    
+}
